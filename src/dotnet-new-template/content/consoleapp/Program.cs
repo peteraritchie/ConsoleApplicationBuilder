d3f2ed76fe8ca0f9ad3b9e2010f1818554ceb6c4
Peter Ritchie <peteraritchie@users.noreply.github.com>
2025-01-15 12:34:33 -0500

    Added dotnet new template and pipeline

---
+++ src/dotnet-new-template/content/consoleapp/Program.cs
@@ -0,0 +1,67 @@
+﻿#if (!csharpFeature_ImplicitUsings)
+using System;
+#endif
+using Microsoft.Extensions.Logging;
+
+using Pri.ConsoleApplicationBuilder;
+
+#if (csharpFeature_FileScopedNamespaces)
+namespace Company.ConsoleApplication1;
+
+#if (csharp10orLater)
+class Program(ILogger<Program> logger)
+{
+#else
+class Program
+{
+    public Program(ILogger<Program> logger)
+    {
+        this.logger = logger;
+    }
+
+    private readonly ILogger<Program> logger;
+
+#endif
+    static void Main(string[] args)
+    {
+        var builder = ConsoleApplication.CreateBuilder(args);
+        var program = builder.Build<Program>();
+        program.Run();
+    }
+
+    private void Run()
+    {
+        logger.LogInformation("Hello, World!");
+    }
+}
+#else
+namespace Company.ConsoleApplication1
+{
+#if (csharp10orLater)
+    class Program(ILogger<Program> logger)
+    {
+#else
+    class Program
+    {
+        public Program(ILogger<Program> logger)
+        {
+            this.logger = logger;
+        }
+
+        private readonly ILogger<Program> logger;
+
+#endif
+        static void Main(string[] args)
+        {
+            var builder = ConsoleApplication.CreateBuilder(args);
+            var program = builder.Build<Program>();
+            program.Run();
+        }
+
+        private void Run()
+        {
+            logger.LogInformation("Hello, World!");
+        }
+    }
+}
+#endif
